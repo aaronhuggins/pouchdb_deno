42747b11108ccab70a2e60c5a709758bdee39f06
Aaron Huggins <aaron.huggins@gmail.com>
2022-02-02 12:30:09 -0600

    adjust types for export

--- modules/pouchdb_types/pouchdb-core/index.d.ts
+++ modules/pouchdb_types/pouchdb-core/index.d.ts
@@ -9,7 +9,7 @@
 // TypeScript Version: 2.3
 
 /// <reference types="../debug/index.d.ts" />
-/// <reference types="../pouchdb-find/index.d.ts" />
+// TODO: Candidate for removal; may not be required in Deno. /// <reference types="../pouchdb-find/index.d.ts" />
 // TODO: Candidate for removal; may not be required in Deno. /// <reference lib="dom" />
 
 interface Blob {
@@ -96,6 +96,76 @@ type Fetch = (
 ) => Promise<Response>;
 
 declare namespace PouchDB {
+    namespace Find {
+        interface ConditionOperators {
+            /** Match fields "less than" this one. */
+            $lt?: any;
+
+            /** Match fields "greater than" this one. */
+            $gt?: any;
+
+            /** Match fields "less than or equal to" this one. */
+            $lte?: any;
+
+            /** Match fields "greater than or equal to" this one. */
+            $gte?: any;
+
+            /** Match fields equal to this one. */
+            $eq?: any;
+
+            /** Match fields not equal to this one. */
+            $ne?: any;
+
+            /** True if the field should exist, false otherwise. */
+            $exists?: boolean | undefined;
+
+            /** One of: "null", "boolean", "number", "string", "array", or "object". */
+            $type?: "null" | "boolean" | "number" | "string" | "array" | "object" | undefined;
+
+            /** The document field must exist in the list provided. */
+            $in?: any[] | undefined;
+
+            /** The document field must not exist in the list provided. */
+            $nin?: any[] | undefined;
+
+            /** Special condition to match the length of an array field in a document. Non-array fields cannot match this condition. */
+            $size?: number | undefined;
+
+            /**
+             * Divisor and Remainder are both positive or negative integers.
+             * Non-integer values result in a 404 status.
+             * Matches documents where (field % Divisor == Remainder) is true, and only when the document field is an integer.
+             * [divisor, remainder]
+             */
+            $mod?: [number, number] | undefined;
+
+            /** A regular expression pattern to match against the document field. Only matches when the field is a string value and matches the supplied regular expression. */
+            $regex?: string | undefined;
+
+            /** Matches an array value if it contains all the elements of the argument array. */
+            $all?: any[] | undefined;
+
+            $elemMatch?: ConditionOperators | undefined;
+        }
+        interface CombinationOperators {
+            /** Matches if all the selectors in the array match. */
+            $and?: Selector[] | undefined;
+
+            /** Matches if any of the selectors in the array match. All selectors must use the same index. */
+            $or?: Selector[] | undefined;
+
+            /** Matches if the given selector does not match. */
+            $not?: Selector | undefined;
+
+            /** Matches if none of the selectors in the array match. */
+            $nor?: Selector[] | undefined;
+        }
+        interface Selector extends CombinationOperators {
+            [field: string]: Selector | Selector[] | ConditionOperators | any;
+
+            _id?: string | ConditionOperators | undefined;
+        }
+    }
     namespace Core {
         interface Error {
             /**
