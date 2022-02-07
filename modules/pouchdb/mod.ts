0ece4077890f9ea49e8c84c430717016297ceb0d
Aaron Huggins <aaron.huggins@gmail.com>
2022-02-07 11:38:19 -0600

    Add idb adapter to default pouch export

--- modules/pouchdb/mod.ts
+++ modules/pouchdb/mod.ts
@@ -1,11 +1,16 @@
-/// <reference path="../pouchdb_types/pouchdb-core/index.d.ts" />
-import PouchDB from 'https://cdn.skypack.dev/pouchdb-core@7.2.2'
+/// <reference path="../pouchdb_types/pouchdb-browser/index.d.ts" />
+import PouchDBImpl from 'https://cdn.skypack.dev/pouchdb-core@7.2.2'
+import IDBPouch from '../pouchdb_adapter_idb/mod.ts'
 import HttpPouch from 'https://cdn.skypack.dev/pouchdb-adapter-http@7.2.2'
 import mapreduce from 'https://cdn.skypack.dev/pouchdb-mapreduce@7.2.2'
 import replication from 'https://cdn.skypack.dev/pouchdb-replication@7.2.2'
 
-PouchDB.plugin(HttpPouch)
+// deno-lint-ignore no-explicit-any
+const PouchDBDeno: PouchDB.Static = PouchDBImpl as any
+
+PouchDBDeno.plugin(IDBPouch)
+  .plugin(HttpPouch)
   .plugin(mapreduce)
   .plugin(replication)
 
-export default PouchDB
+export default PouchDBDeno
