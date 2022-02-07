3461aca6c8986869f9f52f18578c50fb3b077c06
Aaron Huggins <aaron.huggins@gmail.com>
2022-02-07 10:57:57 -0600

    Add polyfill for indexeddb

---
+++ modules/indexeddb/mod.ts
@@ -0,0 +1,18 @@
+// deno-lint-ignore-file no-explicit-any
+import './indexeddb_globals.ts'
+import 'https://cdn.skypack.dev/regenerator-runtime@0.13.9'
+import indexeddbshim from 'https://cdn.skypack.dev/indexeddbshim@v9.0.0/dist/indexeddbshim-noninvasive.js'
+import { openDatabase, configureSQLiteDB } from '../websql/mod.ts'
+
+const setGlobalVars = indexeddbshim as (...args: any[]) => void
+
+function createIndexedDB (): IDBFactory {
+  setGlobalVars(null, { win: { openDatabase } })
+
+  return indexedDB
+}
+
+export {
+  createIndexedDB,
+  configureSQLiteDB
+}
