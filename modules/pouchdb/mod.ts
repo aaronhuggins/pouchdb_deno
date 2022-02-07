10067514a7c28715a7afb490ff9d20f7c7dd30b2
Aaron Huggins <aaron.huggins@gmail.com>
2022-02-07 00:03:30 -0600

    Remove deprecated local storage code

--- modules/pouchdb/mod.ts
+++ modules/pouchdb/mod.ts
@@ -3,10 +3,8 @@ import PouchDB from 'https://cdn.skypack.dev/pouchdb-core@7.2.2'
 import HttpPouch from 'https://cdn.skypack.dev/pouchdb-adapter-http@7.2.2'
 import mapreduce from 'https://cdn.skypack.dev/pouchdb-mapreduce@7.2.2'
 import replication from 'https://cdn.skypack.dev/pouchdb-replication@7.2.2'
-import LocalStoragePouch from '../pouchdb_adapter_localstorage/mod.ts'
 
-PouchDB.plugin(LocalStoragePouch)
-  .plugin(HttpPouch)
+PouchDB.plugin(HttpPouch)
   .plugin(mapreduce)
   .plugin(replication)
 
