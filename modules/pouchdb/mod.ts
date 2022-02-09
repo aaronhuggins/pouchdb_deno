b58e1bf04205501bd5fa1130e69e95906ff020ca
Aaron Huggins <aaron.huggins@gmail.com>
2022-02-08 19:16:34 -0600

    Re-order imports

--- modules/pouchdb/mod.ts
+++ modules/pouchdb/mod.ts
@@ -1,10 +1,10 @@
 import PouchDBImpl from 'https://cdn.skypack.dev/pouchdb-core@7.2.2'
-import IDBPouch from '../pouchdb_adapter_idb/mod.ts'
 import HttpPouch from 'https://cdn.skypack.dev/pouchdb-adapter-http@7.2.2'
-import MemoryPouch from '../pouchdb_adapter_memory/mod.ts'
 import find from 'https://cdn.skypack.dev/pouchdb-find@7.2.2'
 import mapreduce from 'https://cdn.skypack.dev/pouchdb-mapreduce@7.2.2'
 import replication from 'https://cdn.skypack.dev/pouchdb-replication@7.2.2'
+import IDBPouch from '../pouchdb_adapter_idb/mod.ts'
+import MemoryPouch from '../pouchdb_adapter_memory/mod.ts'
 import type { PouchDB } from '../pouchdb_types/pouchdb.ts'
 
 // deno-lint-ignore no-explicit-any
