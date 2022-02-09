f5187d99db42f28528f8e6b9d491f2574e711930
Aaron Huggins <aaron.huggins@gmail.com>
2022-02-09 11:02:01 -0600

    Switch all urls to prod imports

--- modules/pouchdb/mod.ts
+++ modules/pouchdb/mod.ts
@@ -1,10 +1,10 @@
 import PouchDBImpl from 'https://cdn.skypack.dev/pin/pouchdb-core@v7.2.2-6nfR7P1D7GA03jBaX1mS/mode=imports/optimized/pouchdb-core.js'
-import HttpPouch from 'https://cdn.skypack.dev/pouchdb-adapter-http@7.2.2'
-import find from 'https://cdn.skypack.dev/pouchdb-find@7.2.2'
-import mapreduce from 'https://cdn.skypack.dev/pouchdb-mapreduce@7.2.2'
-import replication from 'https://cdn.skypack.dev/pouchdb-replication@7.2.2'
+import HttpPouch from 'https://cdn.skypack.dev/pin/pouchdb-adapter-http@v7.2.2-y4aebSvW9D9JkNygjYmK/mode=imports/optimized/pouchdb-adapter-http.js'
+import mapreduce from 'https://cdn.skypack.dev/pin/pouchdb-mapreduce@v7.2.2-kxv4D1wbAjwMDTO3agZu/mode=imports/optimized/pouchdb-mapreduce.js'
+import replication from 'https://cdn.skypack.dev/pin/pouchdb-replication@v7.2.2-Xl6mE36Cc622eXxmQskd/mode=imports/optimized/pouchdb-replication.js'
 import IDBPouch from '../pouchdb_adapter_idb/mod.ts'
 import MemoryPouch from '../pouchdb_adapter_memory/mod.ts'
+import find from '../pouchdb_adapter_find/mod.ts'
 import type { PouchDB } from '../pouchdb_types/pouchdb.ts'
 
 // deno-lint-ignore no-explicit-any
