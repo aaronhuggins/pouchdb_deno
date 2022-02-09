7af868bf84483b2d4b542295ad3b715fac021cc0
Aaron Huggins <aaron.huggins@gmail.com>
2022-02-08 19:15:22 -0600

    Use new types & add find,memory plugins by default

--- modules/pouchdb/mod.ts
+++ modules/pouchdb/mod.ts
@@ -1,15 +1,19 @@
-/// <reference path="../pouchdb_types/pouchdb-browser/index.d.ts" />
 import PouchDBImpl from 'https://cdn.skypack.dev/pouchdb-core@7.2.2'
 import IDBPouch from '../pouchdb_adapter_idb/mod.ts'
 import HttpPouch from 'https://cdn.skypack.dev/pouchdb-adapter-http@7.2.2'
+import MemoryPouch from '../pouchdb_adapter_memory/mod.ts'
+import find from 'https://cdn.skypack.dev/pouchdb-find@7.2.2'
 import mapreduce from 'https://cdn.skypack.dev/pouchdb-mapreduce@7.2.2'
 import replication from 'https://cdn.skypack.dev/pouchdb-replication@7.2.2'
+import type { PouchDB } from '../pouchdb_types/pouchdb.ts'
 
 // deno-lint-ignore no-explicit-any
 const PouchDBDeno: PouchDB.Static = PouchDBImpl as any
 
 PouchDBDeno.plugin(IDBPouch)
   .plugin(HttpPouch)
+  .plugin(MemoryPouch)
+  .plugin(find)
   .plugin(mapreduce)
   .plugin(replication)
 
