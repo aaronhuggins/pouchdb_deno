56e891b9d7bd0a46e27d09dd359a9029ec44a2d5
Aaron Huggins <aaron.huggins@gmail.com>
2022-02-08 19:23:08 -0600

    Add test to validate the default export of pouchdb

---
+++ modules/pouchdb/mod_test.ts
@@ -0,0 +1,6 @@
+import { assertEquals } from 'https://deno.land/std@0.125.0/testing/asserts.ts'
+import PouchDB from './mod.ts'
+
+Deno.test('PouchDB', () => {
+  assertEquals(typeof PouchDB, 'function')
+})
