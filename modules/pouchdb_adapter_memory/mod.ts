9c4788d9ebd93cdbed9db46d044e9c535952076b
Aaron Huggins <aaron.huggins@gmail.com>
2022-02-08 19:13:45 -0600

    Test memory adapter and use new types

--- modules/pouchdb_adapter_memory/mod.ts
+++ modules/pouchdb_adapter_memory/mod.ts
@@ -1,11 +1,10 @@
-import { capturePlugin } from '../pouchdb_deno_utils/capturePlugin.ts'
-/// <reference path="../pouchdb_types/pouchdb-core/index.d.ts" />
+import { capturePlugin } from '../pouchdb_deno_utils/capture_plugin.ts'
 import 'https://cdn.skypack.dev/pouchdb@7.2.2/dist/pouchdb.memory.js'
+import type { PouchDB } from '../pouchdb_types/pouchdb.ts'
 
 // Because we're using the vanilla browser export, the Memory plugin expects
 // a PouchDB global. Function capturePlugin fakes the global and capture the
 // passed plugin for use later with real PouchDB for Deno.
-// deno-lint-ignore no-explicit-any
-const MemoryPouchDeno: (pouchDb: any) => void = capturePlugin('MemoryPouchPlugin')
+const MemoryPouchDeno: (pouchDb: PouchDB.Static) => void = capturePlugin('MemoryPouchPlugin')
 
 export default MemoryPouchDeno
---
+++ modules/pouchdb_adapter_memory/mod_test.ts
@@ -0,0 +1,6 @@
+import { assertEquals } from 'https://deno.land/std@0.125.0/testing/asserts.ts'
+import MemoryPouch from './mod.ts'
+
+Deno.test('MemoryPouch', () => {
+  assertEquals(typeof MemoryPouch, 'function')
+})
