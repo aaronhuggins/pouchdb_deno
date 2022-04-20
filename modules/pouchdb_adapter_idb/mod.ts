5ef07a7c26794568f2d6aa3792c3a07454d61a3b
Aaron Huggins <aaron.huggins@gmail.com>
2022-04-20 15:55:14 -0500

    Wrap the idb adapter with Deno work-around logic

--- modules/pouchdb_adapter_idb/mod.ts
+++ modules/pouchdb_adapter_idb/mod.ts
@@ -1,7 +1,45 @@
-import "https://deno.land/x/indexeddb@v1.1.0/polyfill.ts";
+// deno-lint-ignore-file no-explicit-any
+import "https://deno.land/x/indexeddb@v1.2.0/polyfill.ts";
+import { createIndexedDB } from "https://deno.land/x/indexeddb@v1.2.0/lib/shim.ts";
 import IDBPouchImpl from "https://cdn.skypack.dev/pin/pouchdb-adapter-idb@v7.2.2-en9WxKy5vepCFPLf3Coc/mode=imports/optimized/pouchdb-adapter-idb.js";
 import type { PouchDB } from "../pouchdb_types/pouchdb.ts";
 
-const IDBPouchDeno: (pouchDb: PouchDB.Static) => void = IDBPouchImpl;
+type IDBSignature = ((opts: any, cb: any) => void) & {
+  valid: () => boolean;
+  use_prefix: boolean;
+};
 
-export default IDBPouchDeno;
+const IDBPouchDeno: (pouchDb: PouchDB.Static) => void = IDBPouchImpl;
+let hasIndexedDB = false;
+
+function IDBDenoWrapper(pouchDb: PouchDB.Static): void {
+  IDBPouchDeno(pouchDb);
+
+  const adapterIdb: IDBSignature = (pouchDb as any).adapters.idb;
+  const validIdb = adapterIdb.valid;
+
+  adapterIdb.use_prefix = false;
+
+  function idb(
+    this: any,
+    opts: PouchDB.IdbAdapter.IdbAdapterConfiguration,
+    callback: any,
+  ) {
+    if (!hasIndexedDB) {
+      // Check for system path
+      if (opts && typeof opts.systemPath === "string") {
+        createIndexedDB(true, false, opts.systemPath);
+      } else {
+        createIndexedDB(true, false);
+      }
+      hasIndexedDB = true;
+    }
+    adapterIdb.call(this, opts, callback);
+  }
+
+  idb.valid = validIdb;
+  idb.use_prefix = adapterIdb.use_prefix;
+  (pouchDb as any).adapters.idb = idb;
+}
+
+export default IDBDenoWrapper;
