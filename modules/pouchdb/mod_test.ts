0923e2ee02409de67944f2b8ed69c2042391337b
Aaron Huggins <aaron.huggins@gmail.com>
2022-04-21 13:22:09 -0500

    Add tests for indexeddb

--- modules/pouchdb/mod_test.ts
+++ modules/pouchdb/mod_test.ts
@@ -1,33 +1,43 @@
 import { assertEquals } from "https://deno.land/std@0.136.0/testing/asserts.ts";
 import PouchDB from "./mod.ts";
 
+const indexedDBAdapters: ["idb", "indexeddb"] = ["idb", "indexeddb"];
+const sysFilepath = "./system_db/__sysdb__.sqlite";
+const filepath = "./user_db/foobar.sqlite";
+
 Deno.test("PouchDB", async ({ step }) => {
   await step("is a function", () => {
     assertEquals(typeof PouchDB, "function");
   });
 
-  await step("writes file to path", async () => {
-    const sysFilepath = "./system_db/__sysdb__.sqlite";
-    const filepath = "./user_db/foobar.sqlite";
-    const Factory = PouchDB.defaults({
-      adapter: "idb",
-      prefix: "./user_db/",
-      systemPath: "./system_db/",
+  for (const adapter of indexedDBAdapters) {
+    await step(`adapter ${adapter} writes to file`, async () => {
+      const Factory = PouchDB.defaults({
+        // deno-lint-ignore no-explicit-any
+        adapter: adapter as any,
+        prefix: "./user_db/",
+        systemPath: "./system_db/",
+      });
+      const db = new Factory("foobar");
+      const result = await db.post({ hello: "world" });
+      const actual = await db.get(result.id);
+
+      assertEquals(result.ok, true);
+      assertEquals(actual._id, result.id);
+
+      // The two database backends are incompatible due to scema and indices. Destroy them.
+      await db.destroy();
+      // Call this during tests to clean up any hanging file resources before test finishes. Not for production use.
+      dispatchEvent(new Event("unload"));
+
+      const stat = Deno.statSync(filepath);
+      const stat2 = Deno.statSync(sysFilepath);
+
+      assertEquals(stat.isFile, true);
+      assertEquals(stat2.isFile, true);
     });
-    const db = new Factory("foobar");
+  }
 
-    await db.post({ hello: "world" });
-    await db.destroy();
-    // Call this during tests to clean up any hanging file resources before test finishes. Not for production use.
-    dispatchEvent(new Event("unload"));
-
-    const stat = Deno.statSync(filepath);
-    const stat2 = Deno.statSync(sysFilepath);
-
-    assertEquals(stat.isFile, true);
-    assertEquals(stat2.isFile, true);
-
-    Deno.removeSync(filepath);
-    Deno.removeSync(sysFilepath);
-  });
+  Deno.removeSync(filepath);
+  Deno.removeSync(sysFilepath);
 });
